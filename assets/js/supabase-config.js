3202782862c7879b4a83911e341026348a59bce3
Sohan Rahman <sohanrahman085@gmail.com>
2026-05-24 00:04:16 +0600

    Update supabase-config.js

--- assets/js/supabase-config.js
+++ assets/js/supabase-config.js
@@ -8,9 +8,7 @@ const SUPABASE_URL =
 const SUPABASE_ANON_KEY =
     'sb_publishable_pz9O2PocfjwdFNSlkfnAtw_Hni6wFAh';
 
-// =========================================
 // CREATE SUPABASE CLIENT
-// =========================================
 
 // window.supabase আসে CDN থেকে
 // তাই আলাদা variable use করতে হবে
